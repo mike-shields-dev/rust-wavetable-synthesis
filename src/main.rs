c34da82248cc5900b03a05425ec211f1a0bf9b18
Michael <mr.m.p.shields@gmail.com>
2024-01-16 12:05:37 +0000

    chore: declare wave_table vector and populate with sine wave values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    
+    let wave_table_size = 64;
+    let mut wave_table: Vec<f32> = Vec::with_capacity(wave_table_size);
+
+    for n in 0..wave_table_size {
+        wave_table.push((2.0 * std::f32::consts::PI * n as f32 / wave_table_size as f32).sin());
+    }
 }
