1a8b10e9f1c4be4d735c74a7f45eeb747a5060c7
Michael <mr.m.p.shields@gmail.com>
2024-01-16 12:06:26 +0000

    chore: Create WavetableOscillator struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+struct WavetableOscillator {
+    sample_rate: u32,
+    wave_table: Vec<f32>,
+    index: f32,
+    index_increment: f32,
+}
+
 fn main() {
     let wave_table_size = 64;
     let mut wave_table: Vec<f32> = Vec::with_capacity(wave_table_size);
