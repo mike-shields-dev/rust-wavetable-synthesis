4d336e304c4351121509c9460b746a43d6771662
Michael <mr.m.p.shields@gmail.com>
2024-01-16 12:30:56 +0000

    feat: Init _stream and _stream_handle and play oscillator samples for 5 secs

--- src/main.rs
+++ src/main.rs
@@ -78,4 +78,9 @@ fn main() {
 
     let mut oscillator = WavetableOscillator::new(44100, wave_table);
     oscillator.set_frequency(440.0);
+
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let _result = stream_handle.play_raw(oscillator.convert_samples());
+
+    std::thread::sleep(Duration::from_secs(5));
 }
